9f34c846280a99ef1c42699ba1da1831859f5d7e
Hiren Neema <hneema@tenupsoft.com>
2020-07-27 16:43:46 +0530

    resolved conflicts

---
+++ projects/oc-ng-common-component/src/lib/oc-overall-rating/oc-overall-rating.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { OverallRatingSummary } from 'oc-ng-common-service';
+
+@Component({
+  selector: 'oc-overall-rating',
+  templateUrl: './oc-overall-rating.component.html',
+  styleUrls: ['./oc-overall-rating.component.scss']
+})
+export class OcOverallRatingComponent implements OnInit {
+  
+  @Input() overallReviewLabel='Overall rating';
+  
+  @Input() allReviewSummary :OverallRatingSummary = new OverallRatingSummary();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
