156b0d61d3c800ec074b19acf70b43e92454aece
Dhaval Gohil <dgohil@tenupsoft.com>
2020-08-01 10:42:30 +0530

    resolve conflicts.

--- src/signup.stories.ts
+++ src/signup.stories.ts
@@ -12,10 +12,18 @@ const modules = {
 let signupEmpty = new SellerSignup();
 
 let signupFilled = new SellerSignup();
-signupFilled.uname = "zinal";
-signupFilled.company = "Tenup"
-signupFilled.email = "zmehta@gmail.com"
-signupFilled.password = "Tenup123#"
+if(signupFilled.developerAccount){
+    signupFilled.developerAccount.name = "zinal";
+    signupFilled.developerAccount.email = "zmehta@gmail.com"
+}
+if(signupFilled.developer){
+    signupFilled.developer.name = "Tenup"
+}
+if(signupFilled.extra){
+    signupFilled.extra.password = "Tenup123#"
+}
+signupFilled.isChecked = true;
+
 
 storiesOf('SellerSignup', module)
     .addDecorator(withA11y)
