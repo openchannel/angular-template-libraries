6bde4b6f0a01bc471365cbbde345a16e167c1170
Dhaval Gohil <dgohil@tenupsoft.com>
2020-08-04 18:07:53 +0530

    added authentication service and changes accroding to that.

---
+++ projects/oc-ng-common-service/src/lib/service/authentication.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { SellerSignin } from '../model/seller-signin';
+import { SellerService } from './seller.service';
+import { Router } from '@angular/router';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private sellerService: SellerService,private router : Router) { }
+
+  saveUserAfterLoginSuccess(res,signin:SellerSignin){
+    localStorage.setItem("access_token",res.access_token);
+    if(signin.isChecked){
+      localStorage.setItem("rememberMe","true");
+    }else {
+      localStorage.setItem("rememberMe","false");
+    }
+   }
+
+    /**
+    * This method is responsible for save user profile information. 
+    */
+   saveUserprofileInformation(){
+    this.sellerService.getUserProfileDetails().subscribe(res => {
+        if (res) {
+          localStorage.setItem("email",res.email);
+        }
+        this.router.navigateByUrl("/app-developer");
+    });
+ }
+
+}
--- projects/oc-ng-common-service/src/public-api.ts
+++ projects/oc-ng-common-service/src/public-api.ts
@@ -19,6 +19,7 @@ export * from './lib/model/overall-rating-summary-model';
 export * from './lib/model/oc-review-details-model';
 export * from './lib/model/file-details-model';
 export * from './lib/service/seller.service';
+export * from './lib/service/authentication.service';
 
 export * from './lib/model/seller-app-list';
 
