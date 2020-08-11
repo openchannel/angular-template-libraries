fb2be61d5d48fe25cfc8c8f1a01a42460d35b66f
Moti Prajapati <mprajapati@tenupsoft.com>
2020-08-11 16:41:30 +0530

    added elipsis and camelcase pipe components

---
+++ projects/oc-ng-common-component/src/lib/pipe/ellipsis.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis'
+})
+export class EllipsisPipe implements PipeTransform {
+
+  transform(value: string, limit = 25, ellipsis = '...') {
+    return value.length > limit ? value.substr(0, limit) + ellipsis : value;
+  }
+  
+}
