9882335ab3a7093caf909b5da2b9273560766e41
Julia <denisenko.sk@gmail.com>
2020-09-14 12:12:03 +0300

    Updated oc-input to the custom formControl component

--- projects/oc-ng-common-component/src/lib/oc-input/oc-input.component.ts
+++ projects/oc-ng-common-component/src/lib/oc-input/oc-input.component.ts
@@ -1,45 +1,75 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {DefaultValueAccessor, NgModel} from '@angular/forms';
+import { Component, ElementRef, EventEmitter, forwardRef, HostListener, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, DefaultValueAccessor, NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'oc-input',
   templateUrl: './oc-input.component.html',
-  styleUrls: ['./oc-input.component.scss']
+  styleUrls: ['./oc-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => OcInputComponent),
+    multi: true
+  }],
 })
-export class OcInputComponent implements OnInit {
+export class OcInputComponent implements OnInit, ControlValueAccessor {
 
   @Input() modelName;
   @Input() autoFocus;
   @Input() autocomplete;
-  @Input() placeholder = '';
+  @Input() placeholder: string = '';
+  @Input() class: string = '';
+  @Input() disabled: boolean = false;
+  @Input()
+  set value(val) {
+    this.inputValue = val;
+    this.onChange(this.inputValue);
+  }
 
-  @Output() modelNameChange = new EventEmitter<any>();
-  @Input() disabled = false;
+  public inputValue: string;
 
-  constructor(private el: ElementRef, private control: NgModel) {
+  private onTouched = () => {};
+  private onChange: (value: any) => void = () => {};
+
+  constructor(private el: ElementRef) {
 
   }
 
   ngOnInit(): void {
-
     if (this.autocomplete) {
       this.el.nativeElement.children[0].autocomplete = this.autocomplete;
     }
   }
 
-  ngAfterViewInit() {
-    console.log("ngAfterViewInit invoked");
-    if (this.autoFocus) {
-      setTimeout(() => this.el.nativeElement.children[0].focus(), 0);
-    }
+  changeModelVal(): void {
+    this.onChange(this.inputValue);
   }
-
-  onblur() {
-    (this.control.valueAccessor as DefaultValueAccessor).onTouched();
+  /**
+   * Calls this function with new value. When user wrote something in the component
+   * It needs to know that new data has been entered in the control.
+   */
+  registerOnChange(onChange: (value: any) => void): void {
+    this.onChange = onChange;
   }
-
-  changeModelVal() {
-    this.modelNameChange.emit(this.modelName);
+  /**
+   * Calls this function when user left chosen component.
+   * It needs for validation
+   */
+  registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
   }
-
+  /**
+   * (Optional)
+   * the method will be called by the control when the [disabled] state changes.
+   */
+  setDisabledState(isDisabled: boolean): void {
+  }
+  /**
+   * this method will be called by the control to pass the value to our component.
+   * It is used if the value is changed through the code outside
+   * (setValue or changing the variable that ngModel is tied to),
+   * as well as to set the initial value.
+   */
+  writeValue(obj: any): void {
+    this.inputValue = obj;
+  }
 }
