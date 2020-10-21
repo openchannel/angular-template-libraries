37f0554ddcfb02c298c6a8efa165d827353e4b9f
Julia <denisenko.sk@gmail.com>
2020-10-21 11:55:20 +0300

    Adde full router link to the appGallery component

--- projects/oc-ng-common-component/src/lib/oc-app-galary/oc-app-galary.component.ts
+++ projects/oc-ng-common-component/src/lib/oc-app-galary/oc-app-galary.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { FullAppData } from 'oc-ng-common-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'oc-app-gallery',
@@ -15,7 +16,12 @@ export class OcAppGalleryComponent implements OnInit {
   /** Message that will be shown when no apps */
   @Input() noAppMessage: string = '';
   /** Redirect link for the 'See All' */
-  @Input() seeAllUrl: string | any;
+  @Input() seeAllUrl: string;
+  /**
+   * Object with router link query parameters.
+   * Example: {'some-query': 'some-value'}
+   */
+  @Input() seeAllLinkQuery: any;
   /** Title for the app list preview */
   @Input() appGalleryTitle: string = '';
   /** Description for the app list preview */
@@ -23,10 +29,19 @@ export class OcAppGalleryComponent implements OnInit {
   /** Router link for one app click, will contain 'appId' field */
   @Input() routerLinkForOneApp: string;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
   }
-
+  /** Navigation to router link with params */
+  navigateToSeeAll(): void {
+    if (this.seeAllUrl) {
+      if (this.seeAllLinkQuery) {
+        this.router.navigate([this.seeAllUrl], {queryParams: this.seeAllLinkQuery}).then();
+      } else {
+        this.router.navigate([this.seeAllUrl]).then();
+      }
+    }
+  }
 }
