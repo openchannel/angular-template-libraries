70607508b5492a2e57540de7835d96e60815c557
Julia <denisenko.sk@gmail.com>
2020-10-27 19:09:58 +0200

    Wrote the tests for ocButton component

--- projects/oc-ng-common-component/src/lib/oc-button/oc-button.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-button/oc-button.component.spec.ts
@@ -22,4 +22,34 @@ describe('OcButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain text value', () => {
+    component.text = 'Test button';
+
+    expect(component.text).toEqual('Test button');
+  });
+
+  it('should contain type value and exist', async () => {
+    component.type = 'secondary';
+
+    const button = fixture.nativeElement.querySelector('button');
+    fixture.detectChanges();
+
+    expect(component.type).toEqual('secondary');
+
+    await fixture.whenStable().then(() => {
+      expect(button).toBeTruthy();
+    });
+  });
+
+  it('button should be disabled', async () => {
+    component.disabled = true;
+
+    const button = fixture.nativeElement.querySelector('button');
+    fixture.detectChanges();
+
+    await fixture.whenStable().then(() => {
+      expect(button.disabled).toBeTruthy();
+    });
+  });
 });
