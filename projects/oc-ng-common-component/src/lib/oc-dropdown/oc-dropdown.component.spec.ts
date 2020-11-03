9bac5bce434eabac396066b6e6878bde4d73c0a4
Julia <denisenko.sk@gmail.com>
2020-11-03 12:03:30 +0200

    Wrote tests for ocDropdown component and fixed a typo in stories

--- projects/oc-ng-common-component/src/lib/oc-dropdown/oc-dropdown.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-dropdown/oc-dropdown.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OcDropdownComponent } from './oc-dropdown.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('OcDropdownComponent', () => {
   let component: OcDropdownComponent;
@@ -8,7 +9,8 @@ describe('OcDropdownComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OcDropdownComponent ]
+      declarations: [ OcDropdownComponent ],
+      imports: [NgbModule]
     })
     .compileComponents();
   }));
@@ -17,9 +19,39 @@ describe('OcDropdownComponent', () => {
     fixture = TestBed.createComponent(OcDropdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.options = [
+      {label: 'Select 1', value: 'select1'},
+      {label: 'Select 2', value: 'select2'},
+      {label: 'Select 3', value: 'select3'}
+    ];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change and emit selected value', () => {
+    const dropdownSelect = fixture.nativeElement.querySelector('#dropdownManual');
+
+    spyOn(component.selectedChange, 'emit');
+    dropdownSelect.click();
+    fixture.detectChanges();
+
+    const dropdownButtonSelected = fixture.nativeElement.querySelectorAll('.dropdown-item')[0];
+    dropdownButtonSelected.click();
+
+    expect(component.selected).toEqual({label: 'Select 1', value: 'select1'});
+    expect(component.selectedChange.emit).toHaveBeenCalledWith({label: 'Select 1', value: 'select1'});
+  });
+
+  it('should change a title', () => {
+    component.title = 'Some title of';
+    component.selected = {label: 'Select 1', value: 'select1'};
+
+    const dropdownSelect = fixture.nativeElement.querySelector('#dropdownManual');
+    fixture.detectChanges();
+
+    expect(dropdownSelect.textContent).toContain('Some title of Select 1');
   });
 });
