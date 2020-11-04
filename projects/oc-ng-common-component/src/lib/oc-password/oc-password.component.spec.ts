8a2c708c3844ae968543fa20315b392620884625
Julia <yui@softkit.company>
2020-11-04 20:37:49 +0200

    Wrote tests for the ocPassword component

--- projects/oc-ng-common-component/src/lib/oc-password/oc-password.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-password/oc-password.component.spec.ts
@@ -3,7 +3,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {OcPasswordComponent} from './oc-password.component';
 import {FormsModule, NgModel} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule, By } from '@angular/platform-browser';
 
 describe('OcPasswordComponent', () => {
   let component: OcPasswordComponent;
@@ -26,5 +26,29 @@ describe('OcPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('input should contain placeholder', () => {
+    component.placeholder = 'enter password';
+
+    const input = fixture.debugElement.query(By.css('input')).nativeElement;
+    fixture.detectChanges();
+
+    expect(input.placeholder).toEqual('enter password');
+  });
+
+  it('should emit a value', () => {
+    const input: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    input.value = 'mypass';
+
+    spyOn(component.modelNameChange, 'emit');
+
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    input.dispatchEvent(new Event('input'));
+
+    expect(component.modelName).toEqual('mypass');
+    expect(component.modelNameChange.emit).toHaveBeenCalledWith('mypass');
   });
 });
