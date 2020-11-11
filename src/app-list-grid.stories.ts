ce58dacfa8252ebf5005f276e6c8a60c83531ff5
Julia <yui@softkit.company>
2020-11-11 17:35:05 +0200

    Updated App List Grid data stories

--- src/app-list-grid.stories.ts
+++ src/app-list-grid.stories.ts
@@ -58,41 +58,165 @@ const app: FullAppData = {
   isLive: true
 };
 
-const app1 = {...app};
-app1.description = 'With this plugin you can communicate with your teammates any time';
-app1.summary = 'With this plugin you can communicate with your teammates any time';
-app1.icon = 'https://drive.google.com/u/0/uc?id=19l7Znd-iPPYUhM6zaiQZ01rE2NpkDFyk&export=download';
-app1.name = 'Plugin';
-app1.model[0].type = 'free';
-app1.rating = 3.5;
-app1.reviewCount = 12;
+const app1 = {
+  appId: '344gf-43s3j-gi3423',
+  icon: 'https://drive.google.com/u/0/uc?id=19l7Znd-iPPYUhM6zaiQZ01rE2NpkDFyk&export=download',
+  name: 'Plugin',
+  model: [{
+    type: 'free',
+    price: 0,
+    trial: 1,
+    license: 'single',
+    modelId: '23235hfg4',
+    currency: 'EUR',
+    billingPeriod: 'monthly'
+  }],
+  rating: 3.5,
+  reviewCount: 12,
+  summary: 'With this plugin you can communicate with your teammates any time',
+  description: 'With this plugin you can communicate with your teammates any time',
+  lastUpdated: new Date(),
+  version: 1.1,
+  safeName: ['test-app'],
+  developerId: '44555-3232gvdfdf',
+  submittedDate: new Date(),
+  created: new Date().getMonth() - 2,
+  status: {
+    value: '',
+    lastUpdated: 1.1,
+    modifiedBy: '',
+    reason: ''
+  },
+  statistics: {
+    views: stat,
+    downloads: stat,
+    developerSales: stat,
+    totalSales: stat,
+    ownerships: stat,
+    reviews: stat
+  },
+  isLive: true
+};
 
-const app2 = {...app};
-app2.description = 'Integrate directly with your account and make customer updates a breeze';
-app2.summary = 'Integrate directly with your account and make customer updates a breeze';
-app2.icon = 'https://drive.google.com/u/0/uc?id=1vDDzbS--o_UIgXFE_LmMfVmSAKuprCyb&export=download';
-app2.name = 'Application';
-app2.model[0].price = 11.99;
-app2.rating = 2.4;
-app2.reviewCount = 2;
+const app2 = {
+  appId: '343344gf-43s3j-gi3423',
+  icon: 'https://drive.google.com/u/0/uc?id=1vDDzbS--o_UIgXFE_LmMfVmSAKuprCyb&export=download',
+  name: 'Application',
+  model: [{
+    type: 'recurring',
+    price: 11.99,
+    trial: 1,
+    license: 'single',
+    modelId: '23235hfg4',
+    currency: 'EUR',
+    billingPeriod: 'monthly'
+  }],
+  rating: 2.4,
+  reviewCount: 2,
+  summary: 'Integrate directly with your account and make customer updates a breeze',
+  description: 'Integrate directly with your account and make customer updates a breeze',
+  lastUpdated: new Date(),
+  version: 1.1,
+  safeName: ['test-app'],
+  developerId: '44555-3232gvdfdf',
+  submittedDate: new Date(),
+  created: new Date().getMonth() - 2,
+  status: {
+    value: '',
+    lastUpdated: 1.1,
+    modifiedBy: '',
+    reason: ''
+  },
+  statistics: {
+    views: stat,
+    downloads: stat,
+    developerSales: stat,
+    totalSales: stat,
+    ownerships: stat,
+    reviews: stat
+  },
+  isLive: true
+};
 
-const app3 = {...app};
-app3.description = 'With this plugin you can communicate with your teammates any time';
-app3.summary = 'With this plugin you can communicate with your teammates any time';
-app3.icon = 'https://drive.google.com/u/0/uc?id=1fWkPPXp3tmkYRBy-GtCm_9PkP7fmConE&export=download';
-app3.name = 'Plugin';
-app3.model[0].type = 'free';
-app3.rating = 3.5;
-app3.reviewCount = 12;
+const app3 = {
+  appId: '34ks344gf-43s3j-gi3423',
+  icon: 'https://drive.google.com/u/0/uc?id=1fWkPPXp3tmkYRBy-GtCm_9PkP7fmConE&export=download',
+  name: 'Plugin',
+  model: [{
+    type: 'free',
+    price: 11.99,
+    trial: 1,
+    license: 'single',
+    modelId: '23235hfg4',
+    currency: 'EUR',
+    billingPeriod: 'monthly'
+  }],
+  rating: 3.5,
+  reviewCount: 12,
+  summary: 'With this plugin you can communicate with your teammates any time',
+  description: 'With this plugin you can communicate with your teammates any time',
+  lastUpdated: new Date(),
+  version: 1.1,
+  safeName: ['test-app'],
+  developerId: '44555-3232gvdfdf',
+  submittedDate: new Date(),
+  created: new Date().getMonth() - 2,
+  status: {
+    value: '',
+    lastUpdated: 1.1,
+    modifiedBy: '',
+    reason: ''
+  },
+  statistics: {
+    views: stat,
+    downloads: stat,
+    developerSales: stat,
+    totalSales: stat,
+    ownerships: stat,
+    reviews: stat
+  },
+  isLive: true
+};
 
-const app4 = {...app};
-app4.description = 'Improve and extend your experience right from your own UI';
-app4.summary = 'Improve and extend your experience right from your own UI';
-app4.icon = 'https://drive.google.com/u/0/uc?id=1KipwDw0K8xJC_StaAhsyDTEgcAoVHqDp&export=download';
-app4.name = 'Integration';
-app4.model[0].price = 5.99;
-app4.rating = 4.9;
-app4.reviewCount = 87;
+const app4 = {
+  appId: '35-344gf-43s3j-gi3423',
+  icon: 'https://drive.google.com/u/0/uc?id=1KipwDw0K8xJC_StaAhsyDTEgcAoVHqDp&export=download',
+  name: 'Integration',
+  model: [{
+    type: 'single',
+    price: 5.99,
+    trial: 1,
+    license: 'single',
+    modelId: '23235hfg4',
+    currency: 'EUR',
+    billingPeriod: 'monthly'
+  }],
+  rating: 4.9,
+  reviewCount: 87,
+  summary: 'Integrate directly with your account and make customer updates a breeze',
+  description: 'Integrate directly with your account and make customer updates a breeze',
+  lastUpdated: new Date(),
+  version: 1.1,
+  safeName: ['test-app'],
+  developerId: '44555-3232gvdfdf',
+  submittedDate: new Date(),
+  created: new Date().getMonth() - 2,
+  status: {
+    value: '',
+    lastUpdated: 1.1,
+    modifiedBy: '',
+    reason: ''
+  },
+  statistics: {
+    views: stat,
+    downloads: stat,
+    developerSales: stat,
+    totalSales: stat,
+    ownerships: stat,
+    reviews: stat
+  },
+  isLive: true
+};
 
 export default {
   title: 'App List Grid',
