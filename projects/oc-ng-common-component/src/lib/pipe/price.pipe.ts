2cba519648862142c89c121849b13d183732e124
Julia <yui@softkit.company>
2020-11-11 17:31:46 +0200

    Updated a price pipe

--- projects/oc-ng-common-component/src/lib/pipe/price.pipe.ts
+++ projects/oc-ng-common-component/src/lib/pipe/price.pipe.ts
@@ -6,12 +6,26 @@ import {AppModel} from 'oc-ng-common-service';
 })
 export class PricePipe implements PipeTransform {
 
+  private isoCurrencyCode = {
+    USD: '$',
+    EUR: '€',
+    CNY: '¥',
+    GBP: '£'
+  };
+
   transform(model: AppModel): string {
+    let price: string = '';
+
     if (!model || model.type === 'free') {
-      return 'free';
-    } else if (model.type === 'recurring') {
-      return `${model.price}${model.billingPeriod ? '/' + model.billingPeriod.slice(0, 3) : ''}`;
+      price = 'Free';
+    } else  {
+      price = model.currency ? Object.keys(this.isoCurrencyCode).includes(model.currency) ?
+        this.isoCurrencyCode[model.currency] : '$' : '';
+      price += model.price;
+      if (model.type === 'recurring') {
+        price += '/' + model.billingPeriod.substring(0, 3);
+      }
     }
-    return `${model.price}`;
+    return price;
   }
 }
