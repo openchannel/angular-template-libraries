46bbde7c0975138eb2a5f9a71f530f1bfbeacff7
Julia <yui@softkit.company>
2020-12-22 11:53:04 +0200

    Categories testing WIP

--- projects/oc-ng-common-component/src/lib/oc-app-categories/oc-app-categories.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-app-categories/oc-app-categories.component.spec.ts
@@ -1,6 +1,30 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {OcAppCategoriesComponent} from './oc-app-categories.component';
+import {AppCategoryDetail} from 'oc-ng-common-service';
+import {By} from '@angular/platform-browser';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {CarouselModule} from 'ngx-owl-carousel-o';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appCategory1 = new AppCategoryDetail();
+appCategory1.categoryCardClass = 'category-card';
+appCategory1.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-1.png';
+appCategory1.categoryName = 'All Apps';
+appCategory1.categoryTitleColor = 'orange';
+
+const appCategory2 = new AppCategoryDetail();
+appCategory2.categoryCardClass = 'category-card';
+appCategory2.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-2.png';
+appCategory2.categoryName = 'Analytics';
+appCategory2.categoryTitleColor = 'blue';
+
+const appCategory3 = new AppCategoryDetail();
+appCategory3.categoryCardClass = 'category-card';
+appCategory3.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-3.png';
+appCategory3.categoryName = 'Communication';
+appCategory3.categoryTitleColor = 'green';
 
 describe('OcAppCategoriesComponent', () => {
   let component: OcAppCategoriesComponent;
@@ -8,6 +32,14 @@ describe('OcAppCategoriesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FontAwesomeModule,
+        CarouselModule,
+        BrowserAnimationsModule,
+
+        FontAwesomeModule,
+
+        RouterTestingModule.withRoutes([])],
       declarations: [OcAppCategoriesComponent]
     })
       .compileComponents();
@@ -22,4 +54,25 @@ describe('OcAppCategoriesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('without left scroll button', () => {
+
+    component.data = duplicate(10, appCategory1);
+    fixture.detectChanges();
+
+    const leftScrollButton: HTMLParagraphElement = fixture.debugElement.query(By.css('.carousel-nav.carousel-left')).nativeElement;
+    console.log(leftScrollButton);
+  });
 });
+
+function duplicate<T>(count: number, ... items: T[]): T[] {
+  const result: T[] = [];
+  while (count-- >= 0) {
+    result.push(...items);
+  }
+  return result;
+}
+
+
+
+
--- src/app-categories.stories.ts
+++ src/app-categories.stories.ts
@@ -1,54 +1,74 @@
-import {storiesOf} from '@storybook/angular';
+import {moduleMetadata, storiesOf} from '@storybook/angular';
 import {withA11y} from '@storybook/addon-a11y';
 import {
   OcAppCategoriesComponent,
-  OcCommonLibModule
+  OcCommonLibModule, OcNumberComponent
 } from 'projects/oc-ng-common-component/src/public-api';
 import {AppCategoryDetail} from 'oc-ng-common-service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-const modules = {
-  imports: [OcCommonLibModule]
-};
 const appCategory1 = new AppCategoryDetail();
 appCategory1.categoryCardClass = 'category-card';
 appCategory1.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-1.png';
 appCategory1.categoryName = 'All Apps';
+appCategory1.categoryTitleColor = 'orange';
 
 const appCategory2 = new AppCategoryDetail();
 appCategory2.categoryCardClass = 'category-card';
 appCategory2.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-2.png';
 appCategory2.categoryName = 'Analytics';
+appCategory2.categoryTitleColor = 'blue';
 
 const appCategory3 = new AppCategoryDetail();
 appCategory3.categoryCardClass = 'category-card';
 appCategory3.categoryLogo = 'https://stage1-philips-market-test.openchannel.io/assets/img/item-3.png';
 appCategory3.categoryName = 'Communication';
+appCategory3.categoryTitleColor = 'green';
 
-storiesOf('App Categories', module)
-  .addDecorator(withA11y)
-  .addParameters({
-    component: OcAppCategoriesComponent,
-  })
-  .add('Empty', () => ({
-    component: OcAppCategoriesComponent,
-    moduleMetadata: modules,
-    props: {
-      categoryHeaderTitle: 'Categories to Explore',
-      data: [],
-      noDataMsg: 'No Category Found'
-    }
-  })).add('Some', () => ({
+const modules = {
+  imports: [OcCommonLibModule, RouterTestingModule.withRoutes([]), BrowserAnimationsModule],
+};
+
+export default {
+  title: 'App Categories',
+  component: OcAppCategoriesComponent,
+  decorators: [
+    moduleMetadata(modules),
+  ],
+};
+
+const AppCategoriesComponent = (args: OcNumberComponent) => ({
   component: OcAppCategoriesComponent,
   moduleMetadata: modules,
-  props: {
-    categoryHeaderTitle: 'Categories to Explore',
-    data: [appCategory1]
+  props: args
+});
+
+export const Empty = AppCategoriesComponent.bind({});
+Empty.args = {
+  categoryHeaderTitle: 'Categories to Explore',
+  data: [],
+  noDataMsg: 'No Category Found'
+}
+
+export const Some = AppCategoriesComponent.bind({});
+Some.args = {
+  categoryHeaderTitle: 'Categories to Explore',
+  categoryRouterLink: 'test/category',
+  data: [appCategory1]
+}
+
+export const All = AppCategoriesComponent.bind({});
+All.args = {
+  categoryHeaderTitle: 'Categories to Explore',
+  categoryRouterLink: 'test/category',
+  data: duplicate(5, appCategory1, appCategory2, appCategory3)
+}
+
+function duplicate<T>(count: number, ... items: T[]): T[] {
+  const result: T[] = [];
+  while (count-- >= 0) {
+    result.push(...items);
   }
-})).add('All', () => ({
-  component: OcAppCategoriesComponent,
-  moduleMetadata: modules,
-  props: {
-    categoryHeaderTitle: 'Categories to Explore',
-    data: [appCategory1, appCategory2, appCategory3]
-  }
-}));
+  return result;
+}
