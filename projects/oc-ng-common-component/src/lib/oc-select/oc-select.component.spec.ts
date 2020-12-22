5e109edfd641e203ff17b6c859df0a153ac60a8d
Alex Tkachenko <alex@softkit.company>
2020-12-22 15:27:44 +0200

    updated select

--- projects/oc-ng-common-component/src/lib/oc-select/oc-select.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-select/oc-select.component.spec.ts
@@ -4,6 +4,7 @@ import {OcSelectComponent} from './oc-select.component';
 import {FormsModule, NgModel} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { BrowserModule, By } from '@angular/platform-browser';
+import {NgbDropdownItem, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 describe('OcSelectComponent', () => {
   let component: OcSelectComponent;
@@ -13,7 +14,7 @@ describe('OcSelectComponent', () => {
     TestBed.configureTestingModule({
       declarations: [OcSelectComponent],
       providers: [NgModel],
-      imports: [FormsModule, CommonModule, BrowserModule]
+      imports: [NgbModule, CommonModule, BrowserModule]
     })
       .compileComponents();
   }));
@@ -43,22 +44,22 @@ describe('OcSelectComponent', () => {
   it('should contain value in select', async () => {
     component.writeValue('item1');
 
-    const select = fixture.debugElement.query(By.css('select')).nativeElement;
+    const select = fixture.debugElement.query(By.css('.text-dark')).nativeElement;
     fixture.detectChanges();
 
     await fixture.whenStable().then(() => {
-      expect(select.value).toContain('item1');
+      expect(select.textContent).toContain('item1');
     });
   });
 
   it('input should be disabled', async () => {
     component.setDisabledState(true);
 
-    const select: HTMLSelectElement = fixture.debugElement.query(By.css('select')).nativeElement;
+    const button: HTMLSelectElement = fixture.debugElement.query(By.css('.btn')).nativeElement;
     fixture.detectChanges();
 
     await fixture.whenStable().then(() => {
-      expect(select.disabled).toBeTruthy();
+      expect(button.disabled).toBeTruthy();
     });
   });
 
@@ -68,10 +69,9 @@ describe('OcSelectComponent', () => {
 
     fixture.detectChanges();
 
-    const select: HTMLSelectElement = fixture.debugElement.query(By.css('select')).nativeElement;
-    select.value = select.options[0].value;
+    const button: HTMLSelectElement = fixture.debugElement.query(By.directive(NgbDropdownItem)).nativeElement;
 
-    select.dispatchEvent(new Event('change'));
+    button.dispatchEvent(new Event('click'));
 
     expect(onChangeFunc).toHaveBeenCalled();
     expect(onChangeFunc.mock.calls[0][0]).toBe('item1');
@@ -83,10 +83,9 @@ describe('OcSelectComponent', () => {
 
     fixture.detectChanges();
 
-    const select: HTMLSelectElement = fixture.debugElement.query(By.css('select')).nativeElement;
-    select.value = select.options[0].value;
+    const button: HTMLSelectElement = fixture.debugElement.query(By.css('.btn')).nativeElement;
 
-    select.dispatchEvent(new Event('blur'));
+    button.dispatchEvent(new Event('blur'));
 
     expect(onTouchedFunc).toHaveBeenCalled();
   });
