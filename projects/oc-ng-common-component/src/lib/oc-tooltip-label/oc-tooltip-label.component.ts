d3956e445467d6c4c0aeb2b822ef065f6ddf0f24
Julia <yui@softkit.company>
2021-01-26 18:14:10 +0200

    Updated tooltip label component

--- projects/oc-ng-common-component/src/lib/oc-tooltip-label/oc-tooltip-label.component.ts
+++ projects/oc-ng-common-component/src/lib/oc-tooltip-label/oc-tooltip-label.component.ts
@@ -18,7 +18,7 @@ export class OcTooltipLabelComponent implements OnInit {
   /**
    * infoTitleIconCsv (optional) - icon for showing description.
    */
-  @Input() infoTitleIconCsv: string = null;
+  @Input() infoTitleIconCsv: string = '../../../assets/img/info.svg';
   /** Set global classes for label */
   @Input() labelClass: string = '';
   constructor() {
