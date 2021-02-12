c953e7436c83bff05e483e4345fdcd4cb29f62fa
Julia <yui@softkit.company>
2021-02-12 12:20:03 +0200

    TE-736: Wrote tests for the Profile navbar component

--- projects/oc-ng-common-component/src/lib/oc-profile-navbar/oc-profile-navbar.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/oc-profile-navbar/oc-profile-navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OcProfileNavbarComponent } from './oc-profile-navbar.component';
+import {By} from '@angular/platform-browser';
 
 describe('OcProfileNavbarComponent', () => {
   let component: OcProfileNavbarComponent;
@@ -21,5 +22,30 @@ describe('OcProfileNavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show data', () => {
+    component.username = 'Test Username';
+    component.initials = 'TU';
+    component.role = 'admin';
+    fixture.detectChanges();
+
+    const usernameField = fixture.debugElement.query(By.css('.profile-navbar__username')).nativeElement;
+    const initialsField = fixture.debugElement.query(By.css('.profile-navbar__initials')).nativeElement;
+    const roleField = fixture.debugElement.query(By.css('.profile-navbar__role')).nativeElement;
+
+    expect(usernameField.textContent).toEqual('Test Username');
+    expect(initialsField.textContent).toEqual('TU');
+    expect(roleField.textContent).toEqual('admin');
+  });
+
+  it('should show dropdown', () => {
+    component.username = 'Test Username';
+    fixture.detectChanges();
+    const toggle = fixture.debugElement.query(By.css('#navbarDropdown')).nativeElement;
+    toggle.click();
+
+    const dropdown = fixture.debugElement.query(By.css('.dropdown-item')).nativeElement;
+    expect(dropdown).toBeTruthy();
   });
 });
