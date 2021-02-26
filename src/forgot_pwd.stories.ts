2dc5f64d88e554c9cf63898ba40b3335ba170faa
Julia <yui@softkit.company>
2021-02-26 12:58:45 +0200

    TE-893: updated the story of the Forgot password component

--- src/forgot_pwd.stories.ts
+++ src/forgot_pwd.stories.ts
@@ -14,7 +14,7 @@ const pwdFilled = new UserLoginModel();
 pwdFilled.email = 'zmehta@tenupsoft.com';
 
 export default {
-  title: 'Forgot Password',
+  title: 'Forgot Password [BEM]',
   component: OcForgotPasswordComponent,
   decorators: [
     moduleMetadata(modules),
@@ -49,5 +49,6 @@ ResultPage.args = {
   loginUrl: 'login',
   signupUrl: 'signup',
   companyLogoUrl: './assets/img/logo-company.png',
+  forgotPasswordDoneUrl: './assets/img/email_done.svg',
   showResultPage: true
 };
