ccf2ba3ab9a5b1c12daec9db035045ff33808719
More4 <pavellarionov@softkit.company>
2021-02-26 19:03:07 +0200

    feature/TE-894 tags component

--- src/tags.stories.ts
+++ src/tags.stories.ts
@@ -1,13 +1,13 @@
 import {moduleMetadata} from '@storybook/angular';
 import {OcCommonLibModule} from 'projects/oc-ng-common-component/src/public-api';
-import {OcTagsComponent} from "oc-ng-common-component";
+import {OcTagsComponent} from 'oc-ng-common-component';
 
 const modules = {
     imports: [OcCommonLibModule]
 };
 
 export default {
-    title: 'Tags',
+    title: 'Tags [BEM]',
     component: OcTagsComponent,
     decorators: [
         moduleMetadata(modules),
