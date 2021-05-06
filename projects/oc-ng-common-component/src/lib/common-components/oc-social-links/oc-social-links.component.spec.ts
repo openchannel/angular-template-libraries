e3d259ba8c09a487dd11c2431c96bd9e1c37d8b5
Alex Tkachenko <pavellarionov@softkit.company>
2021-05-06 18:58:01 +0300

    added test for social links component

--- projects/oc-ng-common-component/src/lib/common-components/oc-social-links/oc-social-links.component.spec.ts
+++ projects/oc-ng-common-component/src/lib/common-components/oc-social-links/oc-social-links.component.spec.ts
@@ -1,7 +1,8 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { OcSocialLinksComponent } from './oc-social-links.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import {OcSocialLinksComponent} from './oc-social-links.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from "@angular/platform-browser";
 
 describe('OcSocialLinksComponent', () => {
     let component: OcSocialLinksComponent;
@@ -11,24 +12,35 @@ describe('OcSocialLinksComponent', () => {
         TestBed.configureTestingModule({
             declarations: [OcSocialLinksComponent],
             imports: [RouterTestingModule.withRoutes([])]
-        })
-            .compileComponents();
+        }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(OcSocialLinksComponent);
         component = fixture.componentInstance;
-        component.socialLinks = [
-            {
-                link: 'https://facebook.com',
-                iconSrc: 'assets/img/facebook-icon.svg',
-                iconAlt: 'facebook-icon'
-            }
-        ]
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should receive passed data', () => {
+        component.socialLinks = [
+            {
+                link: 'https://facebook.com',
+                iconSrc: 'assets/img/facebook-icon.svg',
+                iconAlt: 'facebook-icon'
+            },
+            {
+                link: 'https://twitter.com',
+                iconSrc: 'assets/img/twitter-icon.svg',
+                iconAlt: 'twitter-icon'
+            }
+        ]
+
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.queryAll(By.css('.social-links__item')).length).toBe(component.socialLinks.length);
     });
 });
