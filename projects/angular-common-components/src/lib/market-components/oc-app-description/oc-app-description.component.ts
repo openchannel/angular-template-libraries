9cf9b208d5a753c4fbbff30895ef07d41ffc260e
Julia <denisenko.sk@gmail.com>
2021-05-21 19:48:12 +0300

    AT-1105: Fixed expand trigger button text

--- projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.ts
+++ projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'oc-app-description',
     templateUrl: './oc-app-description.component.html',
     styleUrls: ['./oc-app-description.component.scss'],
 })
-export class OcAppDescriptionComponent {
+export class OcAppDescriptionComponent implements OnInit {
     /** App Description text */
     @Input() appDescription: string = '';
     @Input() set header(header: string) {
@@ -18,18 +18,33 @@ export class OcAppDescriptionComponent {
      * Default: 'Show full description'
      */
     @Input() expandDescriptionText: string = 'Show full description';
+    /**
+     * Text that will be shown on expand description button
+     * Default: 'Hide full description'
+     */
+    @Input() collapseDescriptionText: string = 'Hide full description';
     /** Show full description always. Text for expand description will not be shown */
     @Input() showFullDescription: boolean = false;
     /** String with classes that will be applied to the header */
     @Input() headerClass: string;
     /** Header of the App Description section */
     headerText: string = '';
+    descriptionTriggerText: string;
+
     constructor() {}
+
+    ngOnInit(): void {
+        this.checkOfText();
+    }
 
     triggerDescription(): void {
         this.showFullDescription = !this.showFullDescription;
+        this.checkOfText();
+    }
+
+    checkOfText(): void {
         this.showFullDescription
-            ? (this.expandDescriptionText = 'Hide full description')
-            : (this.expandDescriptionText = 'Show full description');
+            ? (this.descriptionTriggerText = this.collapseDescriptionText)
+            : (this.descriptionTriggerText = this.expandDescriptionText);
     }
 }
