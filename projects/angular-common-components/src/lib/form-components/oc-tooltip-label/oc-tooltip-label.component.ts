b1275cdc2af8d4da2f6be2f1dd057bfeeb33d2b5
Alex Tkachenko <pavellarionov@softkit.company>
2021-05-31 16:40:08 +0300

    storybook documentation for oc tooltip label component

--- projects/angular-common-components/src/lib/form-components/oc-tooltip-label/oc-tooltip-label.component.ts
+++ projects/angular-common-components/src/lib/form-components/oc-tooltip-label/oc-tooltip-label.component.ts
@@ -1,30 +1,32 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'oc-tooltip-label',
-  templateUrl: './oc-tooltip-label.component.html',
-  styleUrls: ['./oc-tooltip-label.component.scss']
+    selector: 'oc-tooltip-label',
+    templateUrl: './oc-tooltip-label.component.html',
+    styleUrls: ['./oc-tooltip-label.component.scss']
 })
 export class OcTooltipLabelComponent implements OnInit {
-  /** Label text */
-  @Input() text: string = '';
-  /** Show indicator of required field */
-  @Input() required: boolean = false;
-  /**
-   * description (optional) - Description for title.
-   * Open small modal panel to the right side with this description text.
-   */
-  @Input() description: string = '';
-  /**
-   * infoTitleIconCsv (optional) - icon for showing description.
-   */
-  @Input() infoTitleIconCsv: string = 'assets/angular-common-components/info.svg';
-  /** Set global classes for label */
-  @Input() labelClass: string = '';
-  constructor() {
-  }
+    /** Label text */
+    @Input() text: string = '';
 
-  ngOnInit(): void {
-  }
+    /** Show indicator of required field */
+    @Input() required: boolean = false;
 
+    /**
+     * description (optional) - Description for title.
+     * Open small modal panel to the right side with this description text.
+     */
+    @Input() description: string = '';
+
+    /**
+     * infoTitleIconCsv (optional) - icon for showing description.
+     */
+    @Input() infoTitleIconCsv: string = 'assets/angular-common-components/info.svg';
+
+    /** Set global classes for label */
+    @Input() labelClass: string = '';
+
+    constructor() {}
+
+    ngOnInit(): void {}
 }
