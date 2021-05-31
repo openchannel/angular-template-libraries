4cb40208aeb238a17ad8364a547f28c0dd7faf44
Julia <yui@softkit.company>
2021-05-31 13:30:07 +0300

    Cleared merge conflicts

--- projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.spec.ts
+++ projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.spec.ts
@@ -6,7 +6,6 @@ import { OcCommonLibModule } from '../../common-components/';
 import { By } from '@angular/platform-browser';
 
 describe('OcAppDescriptionComponent', () => {
-<<<<<<< HEAD
     let component: OcAppDescriptionComponent;
     let fixture: ComponentFixture<OcAppDescriptionComponent>;
     let descriptionElement: Element;
@@ -18,26 +17,11 @@ describe('OcAppDescriptionComponent', () => {
             }).compileComponents();
         }),
     );
-=======
-  let component: OcAppDescriptionComponent;
-  let fixture: ComponentFixture<OcAppDescriptionComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [OcAppDescriptionComponent],
-      imports: [FormsModule]
-    })
-      .compileComponents();
-  }));
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
 
     beforeEach(() => {
         fixture = TestBed.createComponent(OcAppDescriptionComponent);
         component = fixture.componentInstance;
-<<<<<<< HEAD
         descriptionElement = fixture.debugElement.query(By.css('.description__text')).nativeElement;
-=======
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
     });
 
     it('should create', () => {
@@ -80,7 +64,6 @@ describe('OcAppDescriptionComponent', () => {
         expect(getDescriptionText()).toEqual('');
     });
 
-<<<<<<< HEAD
     it('switch full description by click', () => {
         setDescriptionText('lorem ipsum');
         setThreshold(800);
@@ -90,34 +73,19 @@ describe('OcAppDescriptionComponent', () => {
         fixture.nativeElement.querySelector('span').click();
         fixture.detectChanges();
         expect(component.showFullDescription).toBeTruthy();
-        expect(descriptionElement.textContent).toEqual(component._appDescription);
+        expect(descriptionElement.textContent).toEqual(component.appDescriptionText);
     });
 
-=======
-  it('switch full description by click', () => {
-    fixture.detectChanges();
-    expect(component.showFullDescription).toBeFalsy();
-    fixture.nativeElement.querySelector('span').click();
-    fixture.detectChanges();
-    expect(component.showFullDescription).toBeTruthy();
-    expect(fixture.nativeElement.querySelector('span')).toBeNull();
-    });
-
-
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
     function setHeaderText(header: string): void {
         component.header = header;
         fixture.detectChanges();
     }
 
-<<<<<<< HEAD
     function setThreshold(threshold: number): void {
         component.threshold = threshold;
         fixture.detectChanges();
     }
 
-=======
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
     function getHeaderText(): string {
         return fixture.nativeElement.querySelector('h4').innerHTML;
     }
@@ -131,7 +99,3 @@ describe('OcAppDescriptionComponent', () => {
         return fixture.nativeElement.querySelector('p').innerHTML;
     }
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
--- projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.ts
+++ projects/angular-common-components/src/lib/market-components/oc-app-description/oc-app-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, SimpleChanges, ViewChild, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
@@ -6,35 +6,18 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
     templateUrl: './oc-app-description.component.html',
     styleUrls: ['./oc-app-description.component.scss'],
 })
-export class OcAppDescriptionComponent implements OnInit, OnChanges {
+export class OcAppDescriptionComponent implements OnInit {
     @ViewChild('description', { static: true }) description: HTMLParagraphElement;
-
-<<<<<<< HEAD
-  @ViewChild('description', { static: true }) description: HTMLParagraphElement;
-  _appDescription: string
-  /** App Description text */
-  @Input() set appDescription(value: string){
-    this._appDescription = value || '';
-  }
-  get appDescription(){
-    return this._appDescription;
-  }
-  /** Header of the App Description section */
-  _header : string = '';
-  @Input() set header(header: string) {
-    if (header) {
-      this._header = header;
-=======
     /** App Description text */
-    @Input() appDescription: string = '';
+    @Input() set appDescription(value: string) {
+        this.appDescriptionText = value || '';
+    }
     /** Header of the App Description section */
     @Input() set header(header: string) {
         if (header) {
             this.headerText = header;
         }
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
     }
-
     /** Show full description always. Text for expand description will not be shown */
     @Input() showFullDescription: boolean = false;
     /** String with classes that will be applied to the header */
@@ -44,15 +27,9 @@ export class OcAppDescriptionComponent implements OnInit, OnChanges {
     /** Boolean variable to allow 'show more' logic */
     @Input() allowTruncateLogic = true;
 
-<<<<<<< HEAD
-  cutAppDescription: SafeHtml = ""
-
-  constructor(private sanitizer: DomSanitizer) {}
-=======
+    cutAppDescription: SafeHtml = '';
+    appDescriptionText: string;
     headerText: string = '';
-
-    cutAppDescription: SafeHtml;
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
 
     constructor(private sanitizer: DomSanitizer) {}
 
@@ -60,65 +37,34 @@ export class OcAppDescriptionComponent implements OnInit, OnChanges {
         this.rerenderDescription();
     }
 
-<<<<<<< HEAD
-  rerenderDescription(): void {
-    if(this._appDescription && this._appDescription.length){
-      if(this.allowTruncateLogic && this.threshold){
-        let indexBrakeWord: number;
-        let sizeBrakeWord: number;
-        let tagsRegExp = /<[^>]+>/gi;
-        let tagsArray = this._appDescription.match(tagsRegExp);
-        let textArray = this._appDescription.split(tagsRegExp);
-        textArray.pop();
-        textArray.shift();
-        textArray.reduce((acc, cur, i)=>{
-          if(!indexBrakeWord && !sizeBrakeWord && (acc+cur).length>=this.threshold){
-            indexBrakeWord = i;
-            sizeBrakeWord = this.threshold - acc.length;
-          }
-          return acc+cur;
-        })
-        textArray.length = indexBrakeWord+1;
-        textArray[indexBrakeWord] = textArray[indexBrakeWord].slice(0, sizeBrakeWord);
-        this.cutAppDescription = this.sanitizer.bypassSecurityTrustHtml(tagsArray.reduce((acc, curr, i)=>acc+(i-1<=textArray.length-1 ? textArray[i-1] : '')+curr));
-      }
-      if (this.threshold && this._appDescription.length < this.threshold) {
-        this.showFullDescription = true;
-      }
-    } else {
-      this.cutAppDescription = "";
-=======
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes.appDescription.currentValue !== changes.appDescription.previousValue) {
-            this.rerenderDescription();
-        }
-    }
-
     rerenderDescription(): void {
-        if (this.allowTruncateLogic && this.threshold) {
-            let indexBrakeWord: number;
-            let sizeBrakeWord: number;
-            const tagsRegExp = /<[^>]+>/gi;
-            const tagsArray = this.appDescription.match(tagsRegExp);
-            const textArray = this.appDescription.split(tagsRegExp);
-            textArray.pop();
-            textArray.shift();
-            textArray.reduce((acc, cur, i) => {
-                if (!indexBrakeWord && !sizeBrakeWord && (acc + cur).length >= this.threshold) {
-                    indexBrakeWord = i;
-                    sizeBrakeWord = this.threshold - acc.length;
-                }
-                return acc + cur;
-            });
-            textArray.length = indexBrakeWord + 1;
-            textArray[indexBrakeWord] = textArray[indexBrakeWord].slice(0, sizeBrakeWord);
-            this.cutAppDescription = this.sanitizer.bypassSecurityTrustHtml(
-                tagsArray.reduce((acc, curr, i) => acc + (i - 1 <= textArray.length - 1 ? textArray[i - 1] : '') + curr),
-            );
+        if (this.appDescriptionText && this.appDescriptionText.length) {
+            if (this.allowTruncateLogic && this.threshold) {
+                let indexBrakeWord: number;
+                let sizeBrakeWord: number;
+                const tagsRegExp = /<[^>]+>/gi;
+                const tagsArray = this.appDescriptionText.match(tagsRegExp);
+                const textArray = this.appDescriptionText.split(tagsRegExp);
+                textArray.pop();
+                textArray.shift();
+                textArray.reduce((acc, cur, i) => {
+                    if (!indexBrakeWord && !sizeBrakeWord && (acc + cur).length >= this.threshold) {
+                        indexBrakeWord = i;
+                        sizeBrakeWord = this.threshold - acc.length;
+                    }
+                    return acc + cur;
+                });
+                textArray.length = indexBrakeWord + 1;
+                textArray[indexBrakeWord] = textArray[indexBrakeWord].slice(0, sizeBrakeWord);
+                this.cutAppDescription = this.sanitizer.bypassSecurityTrustHtml(
+                    tagsArray.reduce((acc, curr, i) => acc + (i - 1 <= textArray.length - 1 ? textArray[i - 1] : '') + curr),
+                );
+            }
+            if (this.threshold && this.appDescriptionText.length < this.threshold) {
+                this.showFullDescription = true;
+            }
+        } else {
+            this.cutAppDescription = '';
         }
-        if (this.threshold && this.appDescription.length < this.threshold) {
-            this.showFullDescription = true;
-        }
->>>>>>> 5341905580375927066ad1a64f9154cafc965873
     }
 }
