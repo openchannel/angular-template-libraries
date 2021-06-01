6a86e9a67ee2d0cef1c75858dee4168668461688
Alex Tkachenko <pavellarionov@softkit.company>
2021-06-01 14:04:55 +0300

    storybook documentation for oc video url component

--- projects/angular-common-components/src/lib/form-components/oc-video-url/oc-video-url.component.ts
+++ projects/angular-common-components/src/lib/form-components/oc-video-url/oc-video-url.component.ts
@@ -1,28 +1,36 @@
-import {Component, forwardRef, Input} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'oc-video-url',
     templateUrl: './oc-video-url.component.html',
     styleUrls: ['./oc-video-url.component.scss'],
-    providers: [{
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: forwardRef(() => OcVideoUrlComponent),
-        multi: true
-    }],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => OcVideoUrlComponent),
+            multi: true,
+        },
+    ],
 })
 export class OcVideoUrlComponent implements ControlValueAccessor {
     /**
      * Placeholder text for video url input.
+     * @type {string}.
      * Default empty.
      */
     @Input() placeholder: string = '';
 
-    /** List of classes which can be attached to the current list. */
+    /**
+     * List of classes which can be attached to the current list.
+     * @type {string}.
+     * Default empty.
+     */
     @Input() class: string = '';
 
     /**
-     * Set 'disable' state for input.
+     * Sets 'disable' state for input.
+     * @type {boolean}.
      * Default false.
      */
     @Input() disabled: boolean = false;
@@ -34,7 +42,8 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
     @Input() inputType: string = 'text';
 
     /**
-     * Video url setter. Takes a string.
+     * Video url setter.
+     * @type {string}.
      * Calls onChange() and verifyVideoUrl() methods.
      * Required.
      */
@@ -46,9 +55,6 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
 
     videoUrl: string;
     isValidUrl: boolean = false;
-
-    private onTouched = () => {};
-    private onChange: (value: any) => void = () => {};
 
     constructor() {}
 
@@ -78,16 +84,16 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
     }
 
     /**
-     * Calls this function with new value. When user wrote something in the component
-     * It needs to know that new data has been entered in the control.
+     * Calls this method with new value, when a user writes something in the component.
+     * It needs to be checked that new data has entered the control.
      */
     registerOnChange(onChange: (value: any) => void): void {
         this.onChange = onChange;
     }
 
     /**
-     * Calls this function when user left chosen component.
-     * It needs for validation
+     * Calls this method when an input is unfocused.
+     * It is needed for validation.
      */
     registerOnTouched(onTouched: () => void): void {
         this.onTouched = onTouched;
@@ -95,7 +101,7 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
 
     /**
      * (Optional)
-     * the method will be called by the control when the [disabled] state changes.
+     * The method will be called by the control when the [disabled] state changes.
      */
     setDisabledState(isDisabled: boolean): void {
         this.disabled = isDisabled;
@@ -103,7 +109,7 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
 
     /**
      * this method will be called by the control to pass the value to our component.
-     * It is used if the value is changed through the code outside
+     * It is used if the value is changed through the code outside the component,
      * (setValue or changing the variable that ngModel is tied to),
      * as well as to set the initial value.
      */
@@ -111,4 +117,7 @@ export class OcVideoUrlComponent implements ControlValueAccessor {
         this.videoUrl = obj;
         this.verifyVideoUrl();
     }
+
+    private onTouched = () => {};
+    private onChange: (value: any) => void = () => {};
 }
