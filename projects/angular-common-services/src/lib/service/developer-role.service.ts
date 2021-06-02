2b879317eb380730fdef36975a5270ead43792d0
Alex <softkit>
2021-06-02 16:22:34 +0300

    AT-1360 created docs for developer role service

--- projects/angular-common-services/src/lib/service/developer-role.service.ts
+++ projects/angular-common-services/src/lib/service/developer-role.service.ts
@@ -5,6 +5,14 @@ import {HttpRequestService} from './http-request-services';
 import {DeveloperRoleResponse} from '../model/api/account-role-model';
 import {OcHttpParams} from '../model/api/http-params-encoder-model';
 
+
+/**
+ * Description: API service for getting Developer Roles.<br>
+
+ * Endpoints:<br>
+
+ * GET 'v2/developerRoles'<br>
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -15,6 +23,18 @@ export class DeveloperRoleService {
   constructor(private httpService: HttpRequestService) {
   }
 
+  /**
+   * 
+   * Description: Get Developer Roles list with pagination
+   * 
+   * @param {number} pageNumber - (optional) Current page index. Starts from >= 1
+   * @param {number} pageLimit - (optional) Count user types into response. Starts from >= 1
+   * @returns {Observable<Page<DeveloperRoleResponse | any>>} `Observable<Page<DeveloperRoleResponse | any>>`
+   * 
+   * * ### Example:
+   * 
+   * `getDeveloperRoles(1, 10)`
+   */
   getDeveloperRoles(pageNumber: number, pageLimit: number): Observable<Page<DeveloperRoleResponse | any>> {
     return this.httpService.get(this.DEVELOPER_ROLES, {
       params: new OcHttpParams()
