41be260b705b093b21d0df8b731453a8bb802e0c
Alex <softkit>
2021-06-02 11:35:20 +0300

    AT-817 created docs for user account types service

--- projects/angular-common-services/src/lib/service/user-account-types.service.ts
+++ projects/angular-common-services/src/lib/service/user-account-types.service.ts
@@ -5,6 +5,18 @@ import { Page } from '../model/api/page.model';
 import { UserAccountTypeModel } from '../model/api/user-type.model';
 import { OcHttpParams } from '../model/api/http-params-encoder-model';
 
+/**
+
+ * Description: API service for getting User Account Types.<br>
+
+ * Endpoints:<br>
+
+ * GET 'v2/userAccountTypes/{type}'<br>
+
+ * GET 'v2/userAccountTypes'<br>
+
+
+ */
 @Injectable({
     providedIn: 'root',
 })
@@ -14,13 +26,35 @@ export class UserAccountTypesService {
     constructor(private httpService: HttpRequestService) {}
 
     /**
-     * Getting user profile fields definition by type
-     * @param type type from the user account data
+     * 
+     * Description: Getting user profile fields definition by type
+     * 
+     * @param {string} type - (required) type from the user account data
+     * @param {any} httpOptions - (optional)
+     * @returns {Observable<UserAccountTypeModel>} Observable<UserAccountTypeModel>
+     * 
+     * * ### Example:
+     *``
+     * getUserAccountType("developer", {"Authorization": "Bearer o8ahsd89has08dha08s7dh"})
+     *``
      */
     getUserAccountType(type: string, httpOptions?: any): Observable<UserAccountTypeModel> {
         return this.httpService.get(`${this.BASE_USER_ACCOUNTS_TYPES}/${type}`, httpOptions);
     }
-
+    /**
+     * 
+     * Description: Get accoung types list with pagination
+     * 
+     * @param {number} pageNumber - (optional) Current page index. Starts from >= 1.
+     * @param {number} limit - (optional) Count types into response. Starts from >= 1.
+     * @param {string} query - (optional) Your specific search query.
+     * @returns {Observable<Page<UserAccountTypeModel>>} Observable<Page<UserAccountTypeModel>>
+     * 
+     * * ### Example:
+     *``
+     * getUserAccountTypes(1,10,"{"name": {"$in":["first", "second"]}}")
+     *``
+     */
     getUserAccountTypes(pageNumber: number, limit: number, query?: string): Observable<Page<UserAccountTypeModel>> {
         const params = new OcHttpParams().append('pageNumber', String(pageNumber)).append('limit', String(limit)).append('query', query);
 
