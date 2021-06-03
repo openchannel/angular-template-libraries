d31491d327bf3514fbe6ebd7afcf7d2d1ebf74fa
Alex <softkit>
2021-06-03 13:49:55 +0300

    AT-810 sonar fix

--- projects/angular-common-services/src/lib/service/http-request-services.ts
+++ projects/angular-common-services/src/lib/service/http-request-services.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Inject, Injectable} from '@angular/core';
 import {Observable, throwError} from 'rxjs';
 import {catchError, mergeMap} from 'rxjs/operators';
