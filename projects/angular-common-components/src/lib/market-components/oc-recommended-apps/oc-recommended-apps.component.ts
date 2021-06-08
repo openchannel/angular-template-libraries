0786ebac8de13e3f6a4e80da687d69525771424b
Alex Tkachenko <pavellarionov@softkit.company>
2021-06-08 18:52:04 +0300

    storybook documentation for recommended apps component

--- projects/angular-common-components/src/lib/market-components/oc-recommended-apps/oc-recommended-apps.component.ts
+++ projects/angular-common-components/src/lib/market-components/oc-recommended-apps/oc-recommended-apps.component.ts
@@ -9,38 +9,58 @@ import { get } from 'lodash';
 })
 export class OcRecommendedAppsComponent {
     /**
-     *  Array of the Recommended apps
-     *  Must consists fields: 'name', 'model',
-     * 'rating', 'reviewCount', 'summary' or 'description'
+     *  Array of the Recommended apps.
+     *  Must consist of fields: 'name', 'model',
+     * 'rating', 'reviewCount', 'summary' or 'description'.
+     * @type {FullAppData[]}.
+     * Default empty.
      */
     @Input() appList: FullAppData[] = [];
+
     /**
-     * Message that will be shown when no apps
+     * Message that will be shown when no apps.
+     * @type {string}.
+     * Default empty.
      */
     @Input() noAppMessage: string = '';
+
     /**
-     * Title for the Recommended apps list. Default 'Recommended Apps'
+     * Title for the Recommended apps list.
+     * @type {string}.
+     * Default 'Recommended Apps'.
      */
     @Input() recommendedAppTitle: string = 'Recommended Apps';
+
     /**
-     * Custom template for the app card
+     * Custom template for the app card.
+     * @type {FullAppData}.
      */
     @Input() customAppCardTemplate: TemplateRef<FullAppData>;
+
     /**
-     * Router link for one app click
+     * Router link for one app when clicked.
+     * @type {string | any}.
      */
     @Input() routerLinkForOneApp: string | any;
+
     /**
      * Key name of the App object which will be chosen like navigation parameter for the Router link.
+     * @type {string}.
      */
     @Input() appNavigationParam: string;
+
     /**
-     * Emitter for click by App card.
+     * Emitter for click on App card.
+     * @type {FullAppData}.
      */
-    @Output() clickByAppCard: EventEmitter<FullAppData> = new EventEmitter<FullAppData>();
+    @Output() readonly clickByAppCard: EventEmitter<FullAppData> = new EventEmitter<FullAppData>();
 
     constructor() {}
 
+    /**
+     * Uses to get a part of an app router link.
+     * Returns app navigation param or empty string value.
+     */
     getAppValueByParameter(app: FullAppData): string {
         if (this.appNavigationParam) {
             return get(app, this.appNavigationParam);
