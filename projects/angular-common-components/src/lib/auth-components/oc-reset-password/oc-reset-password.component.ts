337357980ccb0570d6afd6aadf3e7e5f4bac4f26
Alex Tkachenko <pavellarionov@softkit.company>
2021-06-08 17:03:51 +0300

    storybook documentation reset password component

--- projects/angular-common-components/src/lib/auth-components/oc-reset-password/oc-reset-password.component.ts
+++ projects/angular-common-components/src/lib/auth-components/oc-reset-password/oc-reset-password.component.ts
@@ -7,18 +7,54 @@ import { ComponentsUserResetPassword } from '../models/auth-types.model';
     styleUrls: ['./oc-reset-password.component.scss'],
 })
 export class OcResetPasswordComponent implements OnInit {
+    /**
+     * A source path for company logo.
+     * @type {string}.
+     */
     @Input() companyLogoUrl;
+
+    /**
+     * A variable which determines whether to show or hide button text.
+     * Shows button text if it has no active process.
+     * @type {boolean}.
+     */
     @Input() process;
+
+    /**
+     * Login url for those users who already has an account.
+     * @type {string}.
+     */
     @Input() loginUrl;
+
+    /**
+     * Signup url for those users who has no account yet.
+     * @type {string}.
+     */
     @Input() signupUrl;
+
+    /**
+     * The user password model for signup.
+     * @type {ComponentsUserResetPassword}.
+     */
     @Input() resetModel = new ComponentsUserResetPassword();
-    @Output() submit = new EventEmitter<any>();
+
+    /**
+     * Event emitter that submits form.
+     * Uses ResetPasswordForm.
+     * @type {*}.
+     */
+    @Output() readonly submit = new EventEmitter<any>();
 
     constructor() {}
 
     ngOnInit(): void {}
 
-    submitForm(form): void {
+    /**
+     * Submits a reset password form.
+     * Marks all fields as touched.
+     * Then emits the valid value of the form.
+     */
+    submitForm(form: any): void {
         if (!form.valid || this.process) {
             form.control.markAllAsTouched();
             this.submit.emit(false);
@@ -27,7 +63,11 @@ export class OcResetPasswordComponent implements OnInit {
         }
     }
 
-    onchange(form): void {
+    /**
+     * Calls when form model changes.
+     * Checks form errors and validators.
+     */
+    onchange(form: any): void {
         if (form.form.controls.newPassword.errors && form.form.controls.newPassword.errors.serverErrorValidator) {
             form.form.controls.newPassword.setErrors(null);
         }
