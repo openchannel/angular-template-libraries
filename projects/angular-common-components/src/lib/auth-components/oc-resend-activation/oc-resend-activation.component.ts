56f74241291113e0ff4b905b0a74ff9d42545e3b
Alex Tkachenko <pavellarionov@softkit.company>
2021-06-09 19:11:57 +0300

    AT-864 storybook documentation for resend activation component

--- projects/angular-common-components/src/lib/auth-components/oc-resend-activation/oc-resend-activation.component.ts
+++ projects/angular-common-components/src/lib/auth-components/oc-resend-activation/oc-resend-activation.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentsUserActivationModel } from '../models/auth-types.model';
 
+/**
+ * A resend activation component. A form, which allows user to retry sending account activation to email.
+ * Contains of a header, password field and navigation links.
+ */
 @Component({
     selector: 'oc-resend-activation',
     templateUrl: './oc-resend-activation.component.html',
@@ -11,39 +15,39 @@ export class OcResendActivationComponent {
      * Login url for those users who already has an account.
      * @type {string}.
      */
-    @Input() loginUrl;
+    @Input() loginUrl: string;
 
     /**
      * Signup url for those users who has no account yet.
      * @type {string}.
      */
-    @Input() signupUrl;
+    @Input() signupUrl: string;
 
     /**
      * A source path for company logo.
      * @type {string}.
      */
-    @Input() companyLogoUrl;
+    @Input() companyLogoUrl: string;
 
     /**
      * A variable which determines whether to show or hide button text.
      * Shows button text if it has no active process.
      * @type {boolean}.
      */
-    @Input() process;
+    @Input() process: boolean = false;
 
     /**
      * The activation model for signup.
      * @type {ComponentsUserActivationModel}.
      */
-    @Input() activationModel = new ComponentsUserActivationModel();
+    @Input() activationModel: ComponentsUserActivationModel = new ComponentsUserActivationModel();
 
     /**
      * Event emitter that submits form.
      * Uses ResetPasswordForm.
      * @type {*}.
      */
-    @Output() readonly submit = new EventEmitter<any>();
+    @Output() readonly submit: EventEmitter<any> = new EventEmitter<any>();
 
     constructor() {}
 
