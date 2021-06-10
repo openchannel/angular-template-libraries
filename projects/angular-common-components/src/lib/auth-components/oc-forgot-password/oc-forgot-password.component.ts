1aa51a3517b61660d3a3b077aeeba8bc8c4053c1
Alex <softkit>
2021-06-10 16:05:09 +0300

    AT-846 created docs for forgot password component

--- projects/angular-common-components/src/lib/auth-components/oc-forgot-password/oc-forgot-password.component.ts
+++ projects/angular-common-components/src/lib/auth-components/oc-forgot-password/oc-forgot-password.component.ts
@@ -1,28 +1,88 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ComponentsUserLoginModel } from '../models/auth-types.model';
 
+/**
+ * Forgot password component. Represents component with forgot password logic.
+ *
+ * Inputs:
+ * @param {ComponentsUserLoginModel} loginModel
+ * @param {string} loginUrl
+ * @param {string} signupUrl
+ * @param {string} forgotPasswordDoneUrl
+ * @param {boolean} showResultPage
+ * @param {string} companyLogoUrl
+ * @param {boolean} process
+ * <br>
+ * Outputs:
+ * @param {EventEmitter<any>} submit
+ *
+ * @example <oc-forgot-password [loginModel]="{
+ *                                  email: 'admin',
+ *                                  password: 'admin',
+ *                                  isChecked: true
+ *                              }"
+ *                              [loginUrl]="'/login'"
+ *                              [signupUrl]="'/signup'"
+ *                              [forgotPasswordDoneUrl]="'/forgot-password.png'"
+ *                              [showResultPage]="true"
+ *                              [companyLogoUrl]="logo.png"
+ *                              [process]="false"
+ *                              (submit)="onSubmit()"
+ * >
+ */
 @Component({
     selector: 'oc-forgot-password',
     templateUrl: './oc-forgot-password.component.html',
     styleUrls: ['./oc-forgot-password.component.scss'],
 })
-export class OcForgotPasswordComponent implements OnInit {
-    @Input() loginModel = new ComponentsUserLoginModel();
-    @Input() loginUrl;
-    @Input() signupUrl;
+export class OcForgotPasswordComponent {
+    /**
+     * Login data
+     */
+    @Input() loginModel: ComponentsUserLoginModel = new ComponentsUserLoginModel();
+
+    /**
+     * Url for back to login
+     */
+    @Input() loginUrl: string;
+
+    /**
+     * Url for go to sign up
+     */
+    @Input() signupUrl: string;
+
     /** Path to the email sent icon in .svg format */
     @Input() forgotPasswordDoneUrl: string = 'assets/angular-common-components/email_done.svg';
-    @Input() showResultPage;
-    @Input() companyLogoUrl;
-    @Input() process;
+
+    /**
+     * Flag to show result page or not
+     */
+    @Input() showResultPage: boolean;
+
+    /**
+     * URL to company logo
+     */
+    @Input() companyLogoUrl: string;
+
+    /**
+     * Flag to know process go or not
+     */
+    @Input() process: boolean;
+
+    /**
+     * Submit output eventemitter
+     */
     @Output() submit = new EventEmitter<any>();
 
     constructor(private router: Router) {}
 
-    ngOnInit(): void {}
-
-    submitForm(form): void {
+    /**
+     * Submit function if form valid emit 'true' value to components output
+     * @param {NgForm} form
+     */
+    submitForm(form: NgForm): void {
         if (!this.process) {
             if (!form.valid) {
                 form.control.markAllAsTouched();
@@ -32,6 +92,9 @@ export class OcForgotPasswordComponent implements OnInit {
         }
     }
 
+    /**
+     * Navigate back to login page
+     */
     goBackToLogin(): void {
         this.router.navigateByUrl(this.loginUrl).then();
     }
