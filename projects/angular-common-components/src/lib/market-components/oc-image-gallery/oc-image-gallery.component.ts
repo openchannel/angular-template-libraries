0b96e2fceb92d88b9650ab8e8cde65a0e1386aeb
Alex <softkit>
2021-06-10 13:52:42 +0300

    AT-849 created docs for image gallery component

--- projects/angular-common-components/src/lib/market-components/oc-image-gallery/oc-image-gallery.component.ts
+++ projects/angular-common-components/src/lib/market-components/oc-image-gallery/oc-image-gallery.component.ts
@@ -1,6 +1,15 @@
 import { AfterContentInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { GalleryItem } from '@openchannel/angular-common-components/src/lib/common-components';
 
+/**
+ * Image gallery component. Show list of images with title and description.
+ *
+ * Inputs:
+ * @param {GalleryItem[]} gallery
+ * @param {number} maxItems
+ * <br>
+ * @example <oc-image-gallery [gallery]="[{image: '/img.png', title: 'Image', descriptiom: 'Description of image'}]" [maxItems]="5">
+ */
 @Component({
     selector: 'oc-image-gallery',
     templateUrl: './oc-image-gallery.component.html',
@@ -14,14 +23,15 @@ export class OcImageGalleryComponent implements AfterContentInit, OnChanges {
     @Input() set gallery(value: GalleryItem[]) {
         this.mainGallery = [...value];
     }
+
     /** Quantity of images that will be shown */
     @Input() maxItems: number = 3;
 
-    // main input gallery array
+    /** Main input gallery array */
     mainGallery: GalleryItem[] = [];
-    // spliced array
+
+    /** Spliced array */
     displayGallery: GalleryItem[] = [];
-    constructor() {}
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.maxItems && this.maxItems) {
@@ -35,6 +45,9 @@ export class OcImageGalleryComponent implements AfterContentInit, OnChanges {
         this.changeMaxImagesView();
     }
 
+    /**
+     * Get data from main gallery and cut everything above the maximum value
+     */
     private changeMaxImagesView(): void {
         this.displayGallery = [...this.mainGallery];
         this.displayGallery.splice(this.maxItems);
