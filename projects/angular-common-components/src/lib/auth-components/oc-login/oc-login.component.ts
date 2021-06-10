05c6b8c753135dff2626f0c01bfacb0e52c2626b
Alex <softkit>
2021-06-10 11:21:54 +0300

    AT-853 created docs for login component

--- projects/angular-common-components/src/lib/auth-components/oc-login/oc-login.component.ts
+++ projects/angular-common-components/src/lib/auth-components/oc-login/oc-login.component.ts
@@ -1,7 +1,28 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
 import { ComponentsUserLoginModel } from '../models/auth-types.model';
-
+/**
+ * Login component. Represent login page with auth logic.
+ * 
+ * Inputs:
+ * @param {ComponentsUserLoginModel} loginModel - default `new ComponentsUserLoginModel()`
+ * @param {string} loginButtonText - default `'Log in'`
+ * @param {string} forgotPwdUrl
+ * @param {string} signupUrl
+ * @param {string} companyLogoUrl - default `'~@openchannel/angular-common-components/assets/angular-common-components/logo-company.png'`
+ * @param {boolean} process
+ * @param {string} incorrectEmailErrorCode - default `'email_is_incorrect'`
+ * @param {string} notVerifiedEmailErrorCode - default `'email_not_verified'`
+ * <br>
+ * Outputs:
+ * @param {EventEmitter<ComponentsUserLoginModel>} loginModelChange
+ * @param {EventEmitter<any>} submit
+ * @param {EventEmitter<string>} sendActivationLink
+ * 
+ * @example <oc-login [loginModel]="{login: 'admin', password:'password', isChecked: true}" [loginButtonText]="'Some text'"
+ *      [forgotPwdUrl]="/forgot-password" [signupUrl]="/signup" [companyLogoUrl]="/newlogo.png" [process]="false"
+ *      [incorrectEmailErrorCode]="" [notVerifiedEmailErrorCode]="">
+ */
 @Component({
     selector: 'oc-login',
     templateUrl: './oc-login.component.html',
@@ -14,20 +35,22 @@ export class OcLoginComponent {
     @Output() loginModelChange = new EventEmitter<ComponentsUserLoginModel>();
 
     @Input() loginButtonText = 'Log In';
-    @Input() forgotPwdUrl;
-    @Input() signupUrl;
+    @Input() forgotPwdUrl: string;
+    @Input() signupUrl: string;
     @Input() companyLogoUrl = '~@openchannel/angular-common-components/assets/angular-common-components/logo-company.png';
-    @Input() process;
-    @Input() loginType;
+    @Input() process: boolean;
     @Input() incorrectEmailErrorCode = 'email_is_incorrect';
     @Input() notVerifiedEmailErrorCode = 'email_not_verified';
 
     @Output() submit = new EventEmitter<any>();
     @Output() sendActivationLink = new EventEmitter<string>();
 
-    constructor() {}
-
-    submitForm(form): void {
+    /**
+     * Submit fuction emit changed login value check form on validity and submit 'true' if everything is ok.
+     * @param {NgForm} form
+     * @returns void
+     */
+    submitForm(form: NgForm): void {
         if (!this.process) {
             this.loginModelChange.emit(this.loginModel);
             if (!form.valid) {
@@ -38,6 +61,9 @@ export class OcLoginComponent {
         }
     }
 
+    /**
+     * Fuction executed when triggers change event
+     */
     onchange(): void {
         if (this.form.form.controls.email.errors && this.form.form.controls.email.errors.serverErrorValidator) {
             this.form.form.controls.email.setErrors(null);
@@ -47,6 +73,10 @@ export class OcLoginComponent {
         }
     }
 
+    /**
+     * Checks if there are server errors for the whole form
+     * @returns boolean
+     */
     isServerErrorExist(): boolean {
         if (this.form) {
             for (const control of Object.keys(this.form.controls)) {
@@ -58,6 +88,10 @@ export class OcLoginComponent {
         return false;
     }
 
+    /**
+     * Checks if there are server errors for provided model
+     * @returns boolean
+     */
     hasServerError(email: NgModel, errorCode: string): boolean {
         if (email.errors) {
             const serverErrorValidator = email.errors.serverErrorValidator;
@@ -68,6 +102,11 @@ export class OcLoginComponent {
         return false;
     }
 
+    /**
+     * Fuction executed when user pres on activation link
+     * @param {NgModel} email
+     * @returns void
+     */
     onActivationLinkClick(email: NgModel): void {
         this.sendActivationLink.emit(email.value);
         this.onchange();
