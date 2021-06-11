95185a4b0525cb39506a99a94f2fd49122b027d6
Alex <softkit>
2021-06-11 13:33:50 +0300

    AT-852 hotfix after code review

--- projects/angular-common-components/src/lib/common-components/oc-label/oc-label.component.ts
+++ projects/angular-common-components/src/lib/common-components/oc-label/oc-label.component.ts
@@ -1,24 +1,19 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 /**
  * Label component. Represent label template with some variables.
  *
- * Inputs:
- * @param {string} text
- * @param {boolean} required - default `false`
- * @param {string} class
- * 
  * @example <oc-label [text]="`label`" [required]="true" [class]="`label`">
  */
 @Component({
-  selector: 'oc-label',
-  templateUrl: './oc-label.component.html',
-  styleUrls: ['./oc-label.component.scss']
+    selector: 'oc-label',
+    templateUrl: './oc-label.component.html',
+    styleUrls: ['./oc-label.component.scss'],
 })
 export class OcLabelComponent {
-  /** Label text */
-  @Input() text: string = '';
-  /** Show indicator of required field */
-  @Input() required: boolean = false;
-  /** Set global classes for label */
-  @Input() class: string = '';
+    /** Label text */
+    @Input() text: string = '';
+    /** Show indicator of required field */
+    @Input() required: boolean = false;
+    /** Set global classes for label */
+    @Input() class: string = '';
 }
