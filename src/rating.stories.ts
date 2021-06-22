81ac0b6266d3aceb5edb447efa2bc91d47abc8cc
Julia <yui@softkit.company>
2021-06-22 16:55:50 +0300

    AT-511: Updated a story of the Rating component

--- src/rating.stories.ts
+++ src/rating.stories.ts
@@ -1,5 +1,4 @@
-import { storiesOf } from '@storybook/angular';
-import { withA11y } from '@storybook/addon-a11y';
+import { moduleMetadata } from '@storybook/angular';
 import { OcRatingComponent } from '@openchannel/angular-common-components/src/lib/market-components';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularSvgIconModule } from 'angular-svg-icon';
@@ -8,25 +7,34 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 const modules = {
     imports: [AngularSvgIconModule.forRoot(), NgbModule, HttpClientTestingModule],
 };
-storiesOf('Rating [BEM]', module)
-    .addDecorator(withA11y)
-    .addParameters({
-        component: OcRatingComponent,
-    })
-    .add('multi star', () => ({
-        component: OcRatingComponent,
-        moduleMetadata: modules,
-        props: {
-            type: 'multi-star',
-            rating: 4,
-        },
-    }))
-    .add('signle star', () => ({
-        component: OcRatingComponent,
-        moduleMetadata: modules,
-        props: {
-            type: 'single-star',
-            rating: 4.5,
-            reviewCount: 50,
-        },
-    }));
+
+export default {
+    title: 'Rating [BEM]',
+    component: OcRatingComponent,
+    decorators: [moduleMetadata(modules)],
+};
+
+const RatingComponent = (args: OcRatingComponent) => ({
+    component: OcRatingComponent,
+    moduleMetadata: modules,
+    props: args,
+});
+
+export const MultiStar = RatingComponent.bind({});
+MultiStar.args = {
+    type: 'multi-star',
+    rating: 4,
+    disabled: true,
+};
+
+export const SingleStar = RatingComponent.bind({});
+SingleStar.args = {
+    type: 'single-star',
+    rating: 4.5,
+    reviewCount: 50,
+};
+
+export const InteractiveRating = RatingComponent.bind({});
+InteractiveRating.args = {
+    type: 'multi-star',
+};
