f6bd906fac43a8c445c2764f776823187962f536
Julia <yui@softkit.company>
2021-07-14 17:52:20 +0300

    AT-1349: implemented the Radio list component to the Form generator component

--- projects/angular-common-components/src/lib/form-components/model/app-form-model.ts
+++ projects/angular-common-components/src/lib/form-components/model/app-form-model.ts
@@ -1,34 +1,35 @@
 export interface AppFormField {
-  id: string;
-  label: string;
-  description?: string;
-  defaultValue: any;
-  type: string;
-  required?: any;
-  attributes?: AppFormFieldAttributes;
-  options?: any;
-  subFieldDefinitions?: AppFormField [];
-  fields?: AppFormField[];
-  placeholder?: string;
-  category?: string;
+    id: string;
+    label: string;
+    description?: string;
+    defaultValue?: any;
+    type: string;
+    required?: any;
+    attributes?: AppFormFieldAttributes;
+    options?: any;
+    subFieldDefinitions?: AppFormField[];
+    fields?: AppFormField[];
+    placeholder?: string;
+    category?: string;
+    specialType?: string;
 }
 
 export interface AppFormModel {
-  formId?: string;
-  name?: string;
-  createdDate?: number;
-  fields?: AppFormField[];
+    formId?: string;
+    name?: string;
+    createdDate?: number;
+    fields?: AppFormField[];
 }
 
 export interface AppFormFieldAttributes {
-  maxCount?: number;
-  minCount?: number;
-  required?: boolean;
-  maxChars?: number;
-  minChars?: number;
-  min?: number;
-  max?: number;
-  ordering?: 'append' | 'prepend';
-  rowLabel?: string;
+    maxCount?: number;
+    minCount?: number;
+    required?: boolean;
+    maxChars?: number;
+    minChars?: number;
+    min?: number;
+    max?: number;
+    ordering?: 'append' | 'prepend';
+    rowLabel?: string;
+    subType?: string;
 }
-
