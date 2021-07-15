d248d26dec508df91ff172ac23af3c573bade1ac
Julia <yui@softkit.company>
2021-07-15 19:36:05 +0300

    AT-1433: updated interfaces for the Sidebar component. Added documentation

--- projects/angular-common-components/src/lib/common-components/model/components-basic.model.ts
+++ projects/angular-common-components/src/lib/common-components/model/components-basic.model.ts
@@ -13,6 +13,29 @@ export interface Sort {
     values: SortValue[];
 }
 
+/**
+ * Interface for the item of the [Filter]{@link Filter}. Extends the [Select model]{@link SelectModel}
+ * Has new fields:
+ *
+ * `id: string` - unique id of the item.
+ *
+ * `label: string` - main text of the item.
+ *
+ * `sort: string` - sort parameters for sorting the result of an API call.
+ *
+ * `query: string` - query parameters that used to specify and limit the results returned by the API.
+ *
+ * `description: string` - description text of the item.
+ * @example
+ * {
+ *     id: "5565322ae4b0a70b13a4563b",
+ *     label: "Featured",
+ *     sort: "{\"attributes.featured\":-1}",
+ *     query: "{\"status.value\":\"approved\",\"attributes.featured\":\"yes\"}"
+ *     description: "This is the most featured apps",
+ *     checked: false
+ * }
+ */
 export interface FilterValue extends SelectModel {
     id: string;
     label: string;
@@ -21,15 +44,61 @@ export interface FilterValue extends SelectModel {
     description: string;
 }
 
+/**
+ * Interface for the filters, by category, for example.
+ * Has fields:
+ *
+ * `id: string` - unique id of the filter.
+ *
+ * `name: string` - title of the filter.
+ *
+ * `description: string` - description text of the filter.
+ *
+ * `values:` {@link FilterValue} - values of the filter.
+ * @example
+ * {
+ *     id: "bgf34jh3498524gjf",
+ *     name: "Categories",
+ *     description: "Filter for the app categories",
+ *     values: [
+ *         {
+ *             id: "fjh345klf351gb09",
+ *             name: "Analytics",
+ *             description: "",
+ *             values: null,
+ *             expanded: false,
+ *             checked: false,
+ *         },
+ *         {
+ *             id: "fjh345klf891gb09",
+ *             name: "Science",
+ *             description: "",
+ *         }
+ *     ]
+ * }
+ */
 export interface Filter {
+    /** unique id of the filter */
     id: string;
+    /** title of the filter */
     name: string;
+    /** description text of the filter */
     description: string;
+    /** values of the filter */
     values: FilterValue[];
 }
 
+/**
+ * Interface for the default select.
+ * Has fields:
+ *
+ * `label: string` - label text of the select
+ * `checked: boolean` - status that shows selected current item or not
+ */
 export interface SelectModel {
+    /** label text of the select */
     label: string;
+    /** status that shows selected current item or not */
     checked: boolean;
 }
 
@@ -50,9 +119,58 @@ export class DropdownModel<T> {
     }
 }
 
+/**
+ * Interface for the [Sidebar component]{@link OcSidebarComponent}.
+ * Extends the [Filter Value]{@link FilterValue} and [Select model]{@link SelectModel} interfaces.
+ * And has new fields:
+ *
+ * `expanded: boolean` - shows that sidebar filter is expanded or collapsed.
+ *
+ * `icon: string` - path to the icon near an item of the sidebar.
+ * @example
+ * {
+ *     id: "bgf34jh3498524gjf",
+ *     name: "Categories",
+ *     description: "Filter for the app categories",
+ *     expanded: true,
+ *     values: [
+ *         {
+ *             id: "fjh345klf351gb09",
+ *             name: "Analytics",
+ *             description: "",
+ *             values: null,
+ *             expanded: false,
+ *             checked: false,
+ *             icon: "./assets/icons/analytics-category-icon.png"
+ *         },
+ *         {
+ *             id: "fjh345klf891gb09",
+ *             name: "Science",
+ *             description: "",
+ *             expanded: true,
+ *             checked: true,
+ *             icon: "https://some-site.com/science.png"
+ *             values: [
+ *                 {
+ *                     id: "fjh345klf891gb09",
+ *                     name: "Astronomy ",
+ *                     description: "",
+ *                     values: null,
+ *                     icon: "",
+ *                     checked: false
+ *                 }
+ *             ]
+ *         }
+ *     ]
+ * }
+ */
 export interface SidebarValue extends FilterValue {
+    /** items of the sidebar */
     values: SidebarValue[];
+    /** shows that sidebar filter is expanded or collapsed */
     expanded?: boolean;
+    /** path to the icon near an item of the sidebar */
+    icon?: string;
 }
 
 export interface ComponentsPage<T> {
