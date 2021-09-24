7896b59ba9131c23fb365fb98cba21cf08b00db4
Julia <yui@softkit.company>
2021-09-24 21:06:40 +0300

    AT-1504: updated modal view for the Full image component

--- projects/angular-common-components/src/lib/market-components/oc-image-gallery/oc-image-gallery.component.ts
+++ projects/angular-common-components/src/lib/market-components/oc-image-gallery/oc-image-gallery.component.ts
@@ -153,7 +153,6 @@ export class OcImageGalleryComponent implements AfterContentInit, OnChanges {
             });
             mediaModalRef.componentInstance.galleryItems = this.displayGallery;
             mediaModalRef.componentInstance.activeItemIdx = index;
-            mediaModalRef.result.then();
         }
     }
 
