edf84487388c48e83479fa2bd99fd5d2495f3b8d
Julia <yui@softkit.company>
2021-09-27 19:56:49 +0300

    AT-1504: fixed test for the new components

--- projects/angular-common-components/src/lib/common-components/oc-full-image-gallery-view-modal/oc-full-image-gallery-view-modal.component.spec.ts
+++ projects/angular-common-components/src/lib/common-components/oc-full-image-gallery-view-modal/oc-full-image-gallery-view-modal.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OcFullImageGalleryViewModalComponent } from './oc-full-image-gallery-view-modal.component';
+import { MockSvgIconComponent, MockVideoComponent } from '@openchannel/angular-common-components/src/mock/mock';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { GalleryItem } from '@openchannel/angular-common-components/src/lib/common-components';
+
+const imageItem: GalleryItem = {
+    image: 'https://static.zerochan.net/Yakkun.full.1531987.jpg',
+    title: 'Test App Image',
+    description: 'Improve and extend your experience right from your own UI',
+};
 
 describe('OcFullImageGalleryViewModalComponent', () => {
-  let component: OcFullImageGalleryViewModalComponent;
-  let fixture: ComponentFixture<OcFullImageGalleryViewModalComponent>;
+    let component: OcFullImageGalleryViewModalComponent;
+    let fixture: ComponentFixture<OcFullImageGalleryViewModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ OcFullImageGalleryViewModalComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [OcFullImageGalleryViewModalComponent, MockSvgIconComponent, MockVideoComponent],
+            providers: [NgbActiveModal],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OcFullImageGalleryViewModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(OcFullImageGalleryViewModalComponent);
+        component = fixture.componentInstance;
+        component.galleryItems = [imageItem, imageItem, imageItem];
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
