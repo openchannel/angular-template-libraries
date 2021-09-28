7e8e0657918ba15731b847121e6e8031adc9f4cc
Julia <yui@softkit.company>
2021-09-28 12:15:59 +0300

    At-1504: created unit tests for the Full image gallery component

--- projects/angular-common-components/src/lib/common-components/oc-full-image-gallery-view-modal/oc-full-image-gallery-view-modal.component.spec.ts
+++ projects/angular-common-components/src/lib/common-components/oc-full-image-gallery-view-modal/oc-full-image-gallery-view-modal.component.spec.ts
@@ -4,6 +4,7 @@ import { OcFullImageGalleryViewModalComponent } from './oc-full-image-gallery-vi
 import { MockSvgIconComponent, MockVideoComponent } from '@openchannel/angular-common-components/src/mock/mock';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GalleryItem } from '@openchannel/angular-common-components/src/lib/common-components';
+import { By } from '@angular/platform-browser';
 
 const imageItem: GalleryItem = {
     image: 'https://static.zerochan.net/Yakkun.full.1531987.jpg',
@@ -31,5 +32,52 @@ describe('OcFullImageGalleryViewModalComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should hide details', () => {
+        component.showDetails = false;
+        fixture.detectChanges();
+        const detailsBlock = fixture.debugElement.query(By.css('.full-image-gallery-modal__details'));
+        expect(detailsBlock).toBeFalsy();
+    });
+
+    it('should show image and description', () => {
+        const detailsBlock = fixture.debugElement.query(By.css('.full-image-gallery-modal__details')).nativeElement;
+        const image = fixture.debugElement.query(By.css('.full-image-gallery-modal__item-image')).nativeElement;
+        const title = fixture.debugElement.query(By.css('.full-image-gallery-modal__details-title')).nativeElement;
+        const description = fixture.debugElement.query(By.css('.full-image-gallery-modal__details-description')).nativeElement;
+        expect(detailsBlock).toBeTruthy();
+        expect(image).toBeTruthy();
+        expect(title.textContent).toEqual('Test App Image');
+        expect(description.textContent).toEqual(' Improve and extend your experience right from your own UI ');
+    });
+
+    it('should change slide on click', () => {
+        const arrowRight = fixture.debugElement.query(By.css('.full-image-gallery-modal__carousel-nav-right')).nativeElement;
+        const arrowLeft = fixture.debugElement.query(By.css('.full-image-gallery-modal__carousel-nav-left')).nativeElement;
+        arrowRight.click();
+
+        fixture.detectChanges();
+        expect(component.activeItemIdx).toEqual(1);
+
+        arrowLeft.click();
+        fixture.detectChanges();
+        expect(component.activeItemIdx).toEqual(0);
+    });
+
+    it('should change slides be arrow key press', () => {
+        const eventRight = new KeyboardEvent('keyup', {
+            code: 'ArrowRight',
+        });
+        const eventLeft = new KeyboardEvent('keyup', {
+            code: 'ArrowLeft',
+        });
+        window.dispatchEvent(eventRight);
+        fixture.detectChanges();
+        expect(component.activeItemIdx).toEqual(1);
+
+        window.dispatchEvent(eventLeft);
+        fixture.detectChanges();
+        expect(component.activeItemIdx).toEqual(0);
     });
 });
