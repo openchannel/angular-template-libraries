3ba1682eb37e345ea98925b5cac8abb9476c33ec
Pavel <pavellarionov@softkit.company>
2021-11-18 15:57:21 +0200

    feature AT-1565 added country servrice

--- projects/angular-common-services/src/lib/service/country.service.ts
+++ projects/angular-common-services/src/lib/service/country.service.ts
@@ -12,18 +12,26 @@ import { map } from 'rxjs/operators';
     providedIn: 'root',
 })
 export class CountryStateService {
+    countries: any;
+    states: any;
+    
+    constructor(private http: HttpClient) {}
     /**
      * Description: Get countries data.
      * @returns {Observable<any>} `Observable<any>`
      * ### Example
-     * `getCountry();`
+     * `getCountries();`
      */
-
-    countries: any;
-    
-    constructor(private http: HttpClient) {}
-
     getCountries(): Observable<any> {
-        return this.http.get('https://restcountries.com/v3.1/all').pipe(map(result => (this.countries = result)));
+        return this.http.get('https://countriesnow.space/api/v0.1/countries/iso').pipe(map(result => (this.countries = result)));
+    }
+    /**
+     * Description: Get states data.
+     * @returns {Observable<any>} `Observable<any>`
+     * ### Example
+     * `getStates(country);`
+     */
+    getStates(country: any): Observable<any> {
+        return this.http.post('https://countriesnow.space/api/v0.1/countries/states', country).pipe(map(result => (this.states = result)));
     }
 }
