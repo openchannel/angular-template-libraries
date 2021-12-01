df9df9e5e024da30812fc66b4c745d52d9e51915
Alex Tkachenko <atkachenko@softkitit.com>
2021-12-01 18:43:28 +0300

    AT-1622: add headers param for requests in transactions service

--- projects/angular-common-services/src/lib/service/transactions.service.ts
+++ projects/angular-common-services/src/lib/service/transactions.service.ts
@@ -4,9 +4,12 @@ import { HttpRequestService } from './http-request-services';
 import { OcApiPaths } from '../oc-ng-common-service.module';
 import { Page } from '../model/api/page.model';
 import { Transaction } from '../model/api/transaction.model';
+import { HttpHeaders } from '@angular/common/http';
 
 /**
  * Description: API service to work with Transactions.<br>
+ *
+ * [OpenChannel Documentation]{@link https://support.openchannel.io/documentation/api/#426-transactions}
  *
  * Endpoints:<br>
  *
@@ -43,14 +46,15 @@ export class TransactionsService {
      * Description: Returns a transaction by the id
      *
      * @param {string} transactionId - The id of the transaction to be returned
+     * @param {HttpHeaders} headers - (optional) HTTP headers for the request
      * @returns {Observable<Transaction>} `Observable<Transaction>`
      *
      * ### Example
      *
      * `getTransactionById('transaction-id');`
      */
-    getTransactionById(transactionId: string): Observable<Transaction> {
-        return this.httpRequest.get(`${this.apiPaths.transactions}/${transactionId}`);
+    getTransactionById(transactionId: string, headers: HttpHeaders = new HttpHeaders()): Observable<Transaction> {
+        return this.httpRequest.get(`${this.apiPaths.transactions}/${transactionId}`, { headers });
     }
 
     /**
@@ -59,14 +63,15 @@ export class TransactionsService {
      *
      * @param {string} transactionId - The id of the transaction to be updated
      * @param {string} customData - A custom JSON object to attach to this transaction
+     * @param {HttpHeaders} headers - (optional) HTTP headers for the request
      * @returns {Observable<Transaction>} `Observable<Transaction>`
      *
      * ### Example
      *
      * `updateTransactionById('transaction-id', { department: 'billing' });`
      */
-    updateTransactionById(transactionId: string, customData: any): Observable<Transaction> {
-        return this.httpRequest.post(`${this.apiPaths.transactions}/${transactionId}`, { customData });
+    updateTransactionById(transactionId: string, customData: any, headers: HttpHeaders = new HttpHeaders()): Observable<Transaction> {
+        return this.httpRequest.post(`${this.apiPaths.transactions}/${transactionId}`, { customData }, { headers });
     }
 
     /**
@@ -74,13 +79,14 @@ export class TransactionsService {
      * Description: Deletes a transaction by the id
      *
      * @param {string} transactionId - The id of the transaction to be deleted
+     * @param {HttpHeaders} headers - (optional) HTTP headers for the request
      * @returns {Observable<{}>} `Observable<{}>`
      *
      * ### Example
      *
      * `deleteTransactionById('transaction-id');`
      */
-    deleteTransactionById(transactionId: string): Observable<{}> {
-        return this.httpRequest.delete(`${this.apiPaths.transactions}/${transactionId}`);
+    deleteTransactionById(transactionId: string, headers: HttpHeaders = new HttpHeaders()): Observable<{}> {
+        return this.httpRequest.delete(`${this.apiPaths.transactions}/${transactionId}`, { headers });
     }
 }
