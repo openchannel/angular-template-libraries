15716e67e5639afb03c8508bb0ba8b112411ee4f
Andrey Glazkov <aglazkov@softkitit.com>
2022-03-07 20:11:53 +0300

    AT-1674: add default value for richText options

--- projects/angular-common-components/src/lib/form-components/service/form-config.service.ts
+++ projects/angular-common-components/src/lib/form-components/service/form-config.service.ts
@@ -80,6 +80,9 @@ export class FormConfigService {
             position: 'top',
             tooltipPosition: 'right',
         },
+        richText: {
+            options: {},
+        },
     };
 
     config: FormConfig = {};
